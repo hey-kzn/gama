90344e046200cda8573e207b98268f12636efdb2
kzn <45941342+hey-kzn@users.noreply.github.com>
2024-01-04 01:07:30 +0100

    wip: web app

--- apps/gama-web-app/src/main.tsx
+++ apps/gama-web-app/src/main.tsx
@@ -1,20 +1,20 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom/client'
-import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {HomePage} from "./pages/HomePage";
-import {ErrorPage} from "./pages/ErrorPage";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { ErrorPage } from './pages/ErrorPage';
 
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <HomePage />,
-        errorElement: <ErrorPage />
-    }
-])
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- apps/gama-web-app/src/pages/HomePage.tsx
+++ apps/gama-web-app/src/pages/HomePage.tsx
@@ -1,5 +1,19 @@
 export const HomePage = () => {
-    return (
-        <div>Home page</div>
-    )
-}
+  return (
+    <>
+      <header className='flex justify-between items-center h-24 max-w-[1240px] mx-auto'>
+        <h1 className='w-full text-3xl font-bold'>GAMA</h1>
+        <div className='flex space-x-4'>
+          {/* use-navigate pour aller sur LoginPage*/}
+          <div className='p-4 bg-purple-600 rounded-lg font-bold'>Login</div>
+          <button className='p-4 bg-purple-600 rounded-lg font-bold'>Register</button>
+        </div>
+      </header>
+      <div>
+        <div className='max-w-[800px] mt-[150px] w-full h-screen mx-auto text-center flex flex-col'>
+          <h1 className='text-4xl font-bold'>Votre gestion de finance</h1>
+        </div>
+      </div>
+    </>
+  );
+};
