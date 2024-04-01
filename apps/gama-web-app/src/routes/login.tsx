c7ddac7ac9007d51a8e06c87cbda3f621cb184a0
kzn <45941342+hey-kzn@users.noreply.github.com>
2024-04-02 01:14:54 +0200

    wip

--- apps/gama-web-app/src/routes/login.tsx
+++ apps/gama-web-app/src/routes/login.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router';
-import { useEffect, useState } from 'react';
 import { useLogin } from '@/hooks/useLogin';
 import { Button } from '@/components/ui/button';
 
